e939d7b3ccb19a01961aa7001fc2ea8e81ecb890
nehaw04 <nehar.xiaeroor@gmail.com>
2024-06-12 12:38:48 +0530

    Initial commit

---
+++ py tkinter.py
@@ -0,0 +1,19 @@
+from tkinter import *
+from tkinter import messagebox
+import random
+
+root=Tk()
+root.title("TO-DO LIST")
+root.geometry("500x300")
+list_frame=Frame(root)
+list_frame.pack()
+
+listbox=Listbox(list_frame,height=20,width=50)
+listbox.pack(side=LEFT)
+
+scroller=Scrollbar(list_frame)
+scroller.pack(side=RIGHT,fill=X)
+
+
+
+root.mainloop()
